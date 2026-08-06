cdbc4d7b104026266d6b524a37f1d0b10008cd35
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-08-06 11:54:42 +0200

    Add parsing for player ratings and player data

--- src/fantacalcio_scraper/parse/html_parsing.py
+++ src/fantacalcio_scraper/parse/html_parsing.py
@@ -1,6 +1,8 @@
-from bs4 import BeautifulSoup
 import datetime
+import re
 from typing import Any
+
+from bs4 import BeautifulSoup
 
 
 def extract_match_urls(html: str) -> list[str]:
@@ -13,36 +15,59 @@ def extract_match_info(html: str) -> dict[Any]:
     bs = BeautifulSoup(html, "html.parser")
     match_date = bs.select_one(".match-date meta").attrs["content"]
     match_time = bs.select_one(".match-date .hours").get_text()
-    home_team = bs.select_one(".team-home a.team-name meta").attrs["content"]
-    away_team = bs.select_one(".team-away a.team-name meta").attrs["content"]
-    home_score = bs.select_one(".score-home").get_text()
-    away_score = bs.select_one(".score-away").get_text()
     return {
-        "home_team": home_team,
-        "home_score": home_score,
-        "away_score": away_score,
-        "away_team": away_team,
+        "home_team": bs.select_one(".team-home a.team-name meta").get("content"),
+        "home_score": bs.select_one(".score-home").get_text(),
+        "away_score": bs.select_one(".score-away").get_text(),
+        "away_team": bs.select_one(".team-away a.team-name meta").get("content"),
         "match_date": datetime.datetime.strptime(
             f"{match_date} {match_time}", "%Y-%m-%d %H:%M"
         ),
     }
 
-def extract_player_ratings(
-    html: str
-) -> list[dict[Any]]:
-    bs = BeautifulSoup(html, 'html.parser')
-    player_infos = bs.find_all(class_='player-info')
+
+def extract_player_ratings(html: str) -> list[dict[Any]]:
+    bs = BeautifulSoup(html, "html.parser")
+    player_infos = bs.find_all(class_="player-info")
     player_ratings = []
     for player in player_infos:
-        player_ratings.append({
-            "player_name": player.select('.player-name span')[0].get_text()
-        })
+        player_url = player.select_one(".player-name").attrs.get("href")
+        bonus_malus = player.select(".icon.bonus-icon")
+        player_ratings.append(
+            {
+                "player_id": re.search(r"/(\d{1,8})/", player_url).group(1),
+                "player_slug": re.search(r"/([a-z-]+)+/[0-9]+", player_url).group(1),
+                "player_team": re.search(r"/squadre/([a-z]+)/", player_url).group(1),
+                "player_rating": player.select_one(".badge.grade").get_text(),
+                "player_bonus_malus": []
+                if not bonus_malus
+                else [{bm.get("title"): bm.get("data-value")} for bm in bonus_malus],
+            }
+        )
     return player_ratings
-    
+
+
+def extract_player_data(html: str) -> dict:
+    bs = BeautifulSoup(html, "html.parser")
+    return {
+        "player_name": bs.select_one(".h5.player-name").get_text(),
+        "player_team": bs.select_one(".team-name.team-link meta").get("content"),
+        "player_height": bs.select_one("dd[itemprop='height']").get_text(),
+        "player_birthdate": bs.select_one(".birthdate").get_text(),
+        "player_foot": bs.select_one("span[title='Sinistro'], span[title='Destro']").get_text(),
+        "player_nationality": bs.select_one(".nationalities").get_text(),
+        "player_main_role": bs.select_one(".role").get('title'),
+        "player_specific_roles": [role.get('title') for role in bs.select('.role.role-mantra')],
+        "player_value_classic": bs.select_one("li[title='Quotazione classic'] .badge.badge-primary").get_text(),
+        "player_value_mantra": bs.select_one("li[title='Quotazione Mantra'] .badge.badge-alternative").get_text(),
+        "player_vfm_classic": bs.select_one("li[title='FantaValore di Mercato (Classic)'] .badge.badge-primary").get_text(),
+        "player_vfm_mantra": bs.select_one("li[title='FantaValore di Mercato (Mantra)'] .badge.badge-alternative").get_text(),
+    }
+
 
 if __name__ == "__main__":
     with open(
-        "/Users/ale/Desktop/altro.html"
+        "/Users/ale/Desktop/coding/projects/personal/fantacalcio/data/player_2.html"
     ) as file:
         html = file.read()
-    print(extract_player_ratings(html=html))
+    print(extract_player_data(html=html))
