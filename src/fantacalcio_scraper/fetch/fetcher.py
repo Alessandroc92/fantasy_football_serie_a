c58d13ea65f6ff89ec7c2101355ca34a8ad5c830
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-08-14 11:37:04 +0200

    Have lynter to modify code formatting

--- src/fantacalcio_scraper/fetch/fetcher.py
+++ src/fantacalcio_scraper/fetch/fetcher.py
@@ -1,13 +1,22 @@
-from niquests import AsyncSession, AsyncResponse
 import asyncio
 
+from niquests import AsyncResponse, AsyncSession
 
-async def async_request(url: str, proxies: str | None = None) -> AsyncResponse:
-    async with AsyncSession() as session:
+from fantacalcio_scraper import config
+
+
+async def async_request(
+    url: str,
+    proxies: str | None = None,
+    pool_maxsize: int = config.POOL_MAXSIZE,
+) -> AsyncResponse:
+    async with AsyncSession(pool_maxsize=pool_maxsize) as session:
         return await session.get(url)
-    
-    
-async def request_cycle(urls: lists[str], proxies: str | None = None) -> list[AsyncResponse]:
+
+
+async def request_cycle(
+    urls: lists[str], proxies: str | None = None
+) -> list[AsyncResponse]:
     tasks = [async_request(url) for url in urls]
     responses = await asyncio.gather(*tasks)
     return responses
