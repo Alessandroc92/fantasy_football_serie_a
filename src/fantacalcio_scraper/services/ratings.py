65e87e0985e92b586189b5849ef67b4747ba9e4f
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-08-26 11:40:22 +0200

    Add player data and player stats to ratings extract module

--- src/fantacalcio_scraper/services/ratings.py
+++ src/fantacalcio_scraper/services/ratings.py
@@ -1,7 +1,10 @@
-from fantacalcio_scraper.parse import html_parsing
-from fantacalcio_scraper.fetch import fetcher, urls
-from itertools import chain
 import asyncio
+import pprint
+from itertools import chain
+
+from fantacalcio_scraper.fetch import fetcher, urls
+from fantacalcio_scraper.parse import html_parsing
+from fantacalcio_scraper.db import save_data
 
 
 async def extract_ratings(start_season: int, end_season: int):
@@ -24,11 +27,24 @@ async def extract_ratings(start_season: int, end_season: int):
         html_parsing.parse_match_info(match_response.text)
         for match_response in match_responses
     ]
-    player_ratings = [
+    teams = {mi['home_team'] for mi in match_info}.union({mi['away_team'] for mi in match_info})
+    save_data.save_teams(teams=teams)
+    save_data.save_match_info(match_info=match_info)
+    # pprint.pprint(match_info, indent=2)
+    player_ratings = chain.from_iterable([
         html_parsing.parse_player_ratings(match_response.text)
         for match_response in match_responses
     ]
-    print(player_ratings)
+    )
+    player_urls = {pl['player_url'] for pl in player_ratings}
+    player_responses = await fetcher.request_cycle(urls=player_urls)
+    player_data = [
+        html_parsing.parse_player_data(player_response.text) for player_response in player_responses
+    ]
+    pprint.pprint(player_data, indent=2)
+    # save_data.save_players(player_data=list(filter(lambda x: x if x else None,player_data)))
+    # save_data.save_player_ratings(player_ratings=player_ratings)
+    # pprint.pprint(player_ratings, indent=2)
 
 
 if __name__ == "__main__":
