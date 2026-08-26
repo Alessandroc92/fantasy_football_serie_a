69db31205b98efb9ae1d4d1ae90f9ea940f982da
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-08-26 11:38:47 +0200

    Create foreign key relationships and player models

--- src/fantacalcio_scraper/db/models.py
+++ src/fantacalcio_scraper/db/models.py
@@ -1,32 +1,51 @@
-from sqlmodel import SQLModel, Field, PrimaryKeyConstraint, ForeignKey
-from pydantic import UrlConstraints
+from sqlmodel import SQLModel, Field, PrimaryKeyConstraint, ForeignKey, Column
+from sqlalchemy.dialects.postgresql import JSONB
+from pydantic import UrlConstraints, AliasChoices
+from datetime import datetime
 
 
 class Team(SQLModel, table=True):
-    team_name: str = Field(alias=['home_team','away_team'])
+    id: int = Field(alias='id', primary_key=True)
+    team_name: str = Field(unique=True)
     
     
 class Match(SQLModel, table=True):
-    fc_match_id: int
-    home_team: Team
-    away_team: Team
+    fc_match_id: int = Field(primary_key=True)
+    home_team_id: int = Field(foreign_key='team.id')
+    away_team_id: int = Field(foreign_key='team.id')
     home_score: int 
     away_score: int
     match_date: datetime
     
     
 class Rating(SQLModel, table=True):
-    fc_match: Match
-    fc_player_id: int
-    player_slug: str
-    player_team: Team 
+    id: int = Field(alias='id', primary_key=True)
+    fc_match_id: int = Field(foreign_key='match.fc_match_id')
+    fc_player_id: int = Field(foreign_key='player.fc_player_id')
+    team_id: int = Field(foreign_key='team.id')
     player_rating: int
-    player_url: UrlConstraints
-    player_bonus_malus: list[dict]
+    bonus_malus: dict = Field(sa_column=Column(JSONB))
     
 
-class BonusMalus(SQLModel, table=True):
-    rating: ForeignKey
+class Player(SQLModel, table=True):
+    fc_player_id: int = Field(primary_key=True)
+    name: str
+    birthdate: datetime
+    height: int 
+    foot: str 
+    nationality: str
     
 
+class PlayerStats(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    fc_player_id: int = Field(foreign_key='player.fc_player_id')
+    team_id: int = Field(foreign_key='team.id')
+    main_role: str
+    specific_roles: dict = Field(sa_column=Column(JSONB))
+    classic_value: int
+    classic_vfm: int
+    mantra_value: int
+    mantra_vfm: int
+    season: int
+    date: datetime = Field(default_factory=datetime.now)
     
