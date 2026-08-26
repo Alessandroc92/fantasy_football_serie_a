e71b4c78c01c992bd1c5c48e540aaaa375518b00
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-08-26 11:36:59 +0200

    Remove verbose suffix to player data dictionary and add match_id in player ratings

--- src/fantacalcio_scraper/parse/html_parsing.py
+++ src/fantacalcio_scraper/parse/html_parsing.py
@@ -37,12 +37,14 @@ def parse_match_info(html: str) -> dict[Any]:
 def parse_player_ratings(html: str) -> list[dict[Any]]:
     bs = BeautifulSoup(html, "html.parser")
     player_infos = bs.find_all(class_="player-info")
+    match_url = bs.select_one("select#matchControl option[selected]").get("value")
     player_ratings = []
     for player in player_infos:
         player_url = player.select_one(".player-name").attrs.get("href")
         bonus_malus = player.select(".icon.bonus-icon")
         player_ratings.append(
             {   "player_url": player_url,
+                "fc_match_id": re.search(r'/([0-9]+)', match_url).group(1),
                 "fc_player_id": re.search(r"/(\d{1,8})/", player_url).group(1),
                 "player_slug": re.search(r"/([^/]+)/[0-9]+", player_url).group(1),
                 "player_team": re.search(r"/squadre/([a-z]+)/", player_url).group(1),
@@ -57,20 +59,23 @@ def parse_player_ratings(html: str) -> list[dict[Any]]:
 
 def parse_player_data(html: str) -> dict:
     bs = BeautifulSoup(html, "html.parser")
-    return {
-        "player_name": bs.select_one(".h5.player-name").get_text(),
-        "player_team": bs.select_one(".team-name.team-link meta").get("content"),
-        "player_height": bs.select_one("dd[itemprop='height']").get_text(),
-        "player_birthdate": bs.select_one(".birthdate").get_text(),
-        "player_foot": bs.select_one("span[title='Sinistro'], span[title='Destro']").get_text(),
-        "player_nationality": bs.select_one(".nationalities").get_text(),
-        "player_main_role": bs.select_one(".role").get('title'),
-        "player_specific_roles": [role.get('title') for role in bs.select('.role.role-mantra')],
-        "player_value_classic": bs.select_one("li[title='Quotazione classic'] .badge.badge-primary").get_text(),
-        "player_value_mantra": bs.select_one("li[title='Quotazione Mantra'] .badge.badge-alternative").get_text(),
-        "player_vfm_classic": bs.select_one("li[title='FantaValore di Mercato (Classic)'] .badge.badge-primary").get_text(),
-        "player_vfm_mantra": bs.select_one("li[title='FantaValore di Mercato (Mantra)'] .badge.badge-alternative").get_text(),
-    }
+    try:
+        return {
+            "name": bs.select_one(".h5.player-name").get_text(),
+            "team": bs.select_one(".team-name.team-link meta").get("content"),
+            "height": bs.select_one("dd[itemprop='height']").get_text(),
+            "birthdate": bs.select_one(".birthdate").get_text(),
+            "foot": bs.select_one("span[title='Sinistro'], span[title='Destro']").get_text(),
+            "nationality": bs.select_one(".nationalities").get_text(),
+            "main_role": bs.select_one(".role").get('title'),
+            "specific_roles": [role.get('title') for role in bs.select('.role.role-mantra')],
+            "classic_value": bs.select_one("li[title='Quotazione classic'] .badge.badge-primary").get_text(),
+            "mantra_value": bs.select_one("li[title='Quotazione Mantra'] .badge.badge-alternative").get_text(),
+            "classic_vfm": bs.select_one("li[title='FantaValore di Mercato (Classic)'] .badge.badge-primary").get_text(),
+            "mantra_vfm": bs.select_one("li[title='FantaValore di Mercato (Mantra)'] .badge.badge-alternative").get_text(),
+        }
+    except:
+        return None
 
 
 if __name__ == "__main__":
