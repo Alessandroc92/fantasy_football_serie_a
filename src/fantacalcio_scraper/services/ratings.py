e15472da28884975ec42af3797f34a5d1edcecb3
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-08-27 15:16:05 +0200

    Complete the extract data pipeline

--- src/fantacalcio_scraper/services/ratings.py
+++ src/fantacalcio_scraper/services/ratings.py
@@ -27,23 +27,32 @@ async def extract_ratings(start_season: int, end_season: int):
         html_parsing.parse_match_info(match_response.text)
         for match_response in match_responses
     ]
-    teams = {mi['home_team'] for mi in match_info}.union({mi['away_team'] for mi in match_info})
+    teams = {mi["home_team"] for mi in match_info}.union(
+        {mi["away_team"] for mi in match_info}
+    )
+    # # pprint.pprint(match_info, indent=2)
     save_data.save_teams(teams=teams)
     save_data.save_match_info(match_info=match_info)
-    # pprint.pprint(match_info, indent=2)
-    player_ratings = chain.from_iterable([
-        html_parsing.parse_player_ratings(match_response.text)
-        for match_response in match_responses
-    ]
+
+    player_ratings = list(
+        chain.from_iterable(
+            [
+                html_parsing.parse_player_ratings(match_response.text)
+                for match_response in match_responses
+            ]
+        )
     )
-    player_urls = {pl['player_url'] for pl in player_ratings}
+    player_urls = list({pl["url"] for pl in player_ratings})[0:10]
     player_responses = await fetcher.request_cycle(urls=player_urls)
     player_data = [
-        html_parsing.parse_player_data(player_response.text) for player_response in player_responses
+        html_parsing.parse_player_data(player_response.text)
+        for player_response in player_responses
     ]
-    pprint.pprint(player_data, indent=2)
-    # save_data.save_players(player_data=list(filter(lambda x: x if x else None,player_data)))
-    # save_data.save_player_ratings(player_ratings=player_ratings)
+    # pprint.pprint(player_data, indent=2)
+    save_data.save_player_data(
+        player_data=list(filter(lambda x: x if x else None, player_data))
+    )
+    save_data.save_player_ratings(player_ratings=player_ratings)
     # pprint.pprint(player_ratings, indent=2)
 
 
