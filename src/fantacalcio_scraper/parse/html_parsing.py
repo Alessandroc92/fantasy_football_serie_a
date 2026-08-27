dd11b98d6ef0646e26547914c80586da8560ada4
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-08-27 15:15:11 +0200

    Format the structure of parsing functions

--- src/fantacalcio_scraper/parse/html_parsing.py
+++ src/fantacalcio_scraper/parse/html_parsing.py
@@ -23,7 +23,7 @@ def parse_match_info(html: str) -> dict[Any]:
     match_time = bs.select_one(".match-date .hours").get_text()
     match_url = bs.select_one("select#matchControl option[selected]").get("value")
     return {
-        "fc_match_id": re.search(r'/([0-9]+)', match_url).group(1),
+        "fc_match_id": re.search(r"/([0-9]+)", match_url).group(1),
         "home_team": bs.select_one(".team-home a.team-name meta").get("content"),
         "home_score": bs.select_one(".score-home").get_text(),
         "away_score": bs.select_one(".score-away").get_text(),
@@ -43,13 +43,14 @@ def parse_player_ratings(html: str) -> list[dict[Any]]:
         player_url = player.select_one(".player-name").attrs.get("href")
         bonus_malus = player.select(".icon.bonus-icon")
         player_ratings.append(
-            {   "player_url": player_url,
-                "fc_match_id": re.search(r'/([0-9]+)', match_url).group(1),
+            {
+                "url": player_url,
+                "fc_match_id": re.search(r"/([0-9]+)", match_url).group(1),
                 "fc_player_id": re.search(r"/(\d{1,8})/", player_url).group(1),
-                "player_slug": re.search(r"/([^/]+)/[0-9]+", player_url).group(1),
-                "player_team": re.search(r"/squadre/([a-z]+)/", player_url).group(1),
-                "player_rating": player.select_one(".badge.grade").get_text(),
-                "player_bonus_malus": []
+                "slug": re.search(r"/([^/]+)/[0-9]+", player_url).group(1),
+                "team": re.search(r"/squadre/([a-z]+)/", player_url).group(1),
+                "rating": player.select_one(".badge.grade").get_text(),
+                "bonus_malus": []
                 if not bonus_malus
                 else [{bm.get("title"): bm.get("data-value")} for bm in bonus_malus],
             }
@@ -60,28 +61,35 @@ def parse_player_ratings(html: str) -> list[dict[Any]]:
 def parse_player_data(html: str) -> dict | None:
     bs = BeautifulSoup(html, "html.parser")
     try:
+        player_url = bs.select_one(".player-name.player-link").get("href")
         return {
             "name": bs.select_one(".h5.player-name").get_text(),
-            "fc_player_id": re.search(r"/(\d{1,8})/", player_url).group(1),
+            "slug": re.search(r"/([^/]+)/[0-9]+", player_url).group(1),
+            "fc_player_id": re.search(r"/([0-9]{1,10})/", player_url).group(1),
             "team": bs.select_one(".team-name.team-link meta").get("content"),
+            "season": re.search(r"-([0-9]{4})", player_url).group(1),
             "height": bs.select_one("dd[itemprop='height']").get_text(),
             "birthdate": bs.select_one(".birthdate").get_text(),
-            "foot": bs.select_one("span[title='Sinistro'], span[title='Destro']").get_text(),
+            "foot": bs.select_one(
+                "span[title='Sinistro'], span[title='Destro']"
+            ).get_text(),
             "nationality": bs.select_one(".nationalities").get_text(),
-            "main_role": bs.select_one(".role").get('title'),
-            "specific_roles": [role.get('title') for role in bs.select('.role.role-mantra')],
-            "classic_value": bs.select_one("li[title='Quotazione classic'] .badge.badge-primary").get_text(),
-            "mantra_value": bs.select_one("li[title='Quotazione Mantra'] .badge.badge-alternative").get_text(),
-            "classic_vfm": bs.select_one("li[title='FantaValore di Mercato (Classic)'] .badge.badge-primary").get_text(),
-            "mantra_vfm": bs.select_one("li[title='FantaValore di Mercato (Mantra)'] .badge.badge-alternative").get_text(),
+            "main_role": bs.select_one(".role").get("title"),
+            "specific_roles": [
+                role.get("title") for role in bs.select(".role.role-mantra")
+            ],
+            "classic_value": bs.select_one(
+                "li[title='Quotazione classic'] .badge.badge-primary"
+            ).get_text(),
+            "mantra_value": bs.select_one(
+                "li[title='Quotazione Mantra'] .badge.badge-alternative"
+            ).get_text(),
+            "classic_vfm": bs.select_one(
+                "li[title='FantaValore di Mercato (Classic)'] .badge.badge-primary"
+            ).get_text(),
+            "mantra_vfm": bs.select_one(
+                "li[title='FantaValore di Mercato (Mantra)'] .badge.badge-alternative"
+            ).get_text(),
         }
-    except:
+    except Exception:
         return None
-
-
-if __name__ == "__main__":
-    with open(
-        "/Users/ale/Desktop/coding/projects/personal/fantacalcio/data/ratings_3.html"
-    ) as file:
-        html = file.read()
-    print(parse_player_ratings(html=html))
