0e7a267f5b2bf865020e36748b43721768242a75
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-09-08 11:37:04 +0200

    Add retry logic to asnyc_request in case of client-server errors

--- src/fantasy_football_scraper/fetch/fetcher.py
+++ src/fantasy_football_scraper/fetch/fetcher.py
@@ -9,8 +9,15 @@ async def async_request(
     proxies: str | None = None,
     pool_maxsize: int = config.POOL_MAXSIZE,
 ) -> AsyncResponse:
-    async with AsyncSession(pool_maxsize=pool_maxsize) as session:
-        return await session.get(url)
+    async with AsyncSession(pool_maxsize=pool_maxsize, timeout=config.TIMEOUT) as session:
+        status_code = 500
+        while status_code == 500:
+            try:
+                response = await session.get(url)
+                status_code == 200
+                return response
+            except Exception as exe:
+                await asyncio.sleep(2)
 
 
 async def request_cycle(
