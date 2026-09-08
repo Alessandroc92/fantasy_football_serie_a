3f13fc72df34a8a3381e8c4080f85efa9db2f10e
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-09-08 11:40:03 +0200

    Ultimate version of the ratings extraction pipeline

--- src/fantasy_football_scraper/services/ratings.py
+++ src/fantasy_football_scraper/services/ratings.py
@@ -1,4 +1,5 @@
 import asyncio
+import pprint
 from itertools import chain
 
 from fantasy_football_scraper.db import save_data
@@ -6,30 +7,34 @@ from fantasy_football_scraper.fetch import fetcher, urls
 from fantasy_football_scraper.parse import html_parsing
 
 
+
 async def extract_ratings(start_season: int, end_season: int):
     matchday_urls = urls.create_matchday_urls(
-        start_season=start_season, end_season=end_season, max_games=1
+        start_season=start_season, end_season=end_season, n_matchdays=2
     )
+    
     matchday_responses = await fetcher.request_cycle(urls=matchday_urls)
-    match_paths = [
-        html_parsing.parse_match_urls(response.text) for response in matchday_responses
-    ]
     match_urls = list(
         chain.from_iterable(
-            urls.create_matches_urls(matchday_url, match_path)
-            for matchday_url in matchday_urls
-            for match_path in match_paths
+            html_parsing.parse_match_urls(response.text)
+            for response in matchday_responses
+            if response.status_code == 200
         )
     )
     match_responses = await fetcher.request_cycle(urls=match_urls)
-    match_info = [
-        html_parsing.parse_match_info(match_response.text)
-        for match_response in match_responses
-    ]
+
+    match_info = list(
+        filter(
+            lambda x: x if x else None,
+            [
+                html_parsing.parse_match_info(match_response.text)
+                for match_response in match_responses
+            ],
+        )
+    )
     teams = {mi["home_team"] for mi in match_info}.union(
         {mi["away_team"] for mi in match_info}
     )
-    # # pprint.pprint(match_info, indent=2)
     save_data.save_teams(teams=teams)
     save_data.save_match_info(match_info=match_info)
 
@@ -41,7 +46,7 @@ async def extract_ratings(start_season: int, end_season: int):
             ]
         )
     )
-    player_urls = list({pl["url"] for pl in player_ratings})[0:10]
+    player_urls = list({pl["url"] for pl in player_ratings})
     player_responses = await fetcher.request_cycle(urls=player_urls)
     player_data = [
         html_parsing.parse_player_data(player_response.text)
@@ -56,5 +61,7 @@ async def extract_ratings(start_season: int, end_season: int):
 
 
 if __name__ == "__main__":
-    test = asyncio.run(extract_ratings(2026, 2026))
+    start_season = 2027
+    end_season = 2027
+    test = asyncio.run(extract_ratings(start_season=start_season, end_season=end_season))
     print(test)
