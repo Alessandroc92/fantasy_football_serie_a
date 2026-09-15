6053b9ac98eb02eda21e0ebf66f251414f3aef77
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-09-15 12:26:12 +0200

    Move from a multi session to a single session approach with multiple requests

--- src/fantasy_football_scraper/fetch/fetcher.py
+++ src/fantasy_football_scraper/fetch/fetcher.py
@@ -1,28 +1,36 @@
 import asyncio
+import itertools
 
 from fantasy_football_scraper import config
 from niquests import AsyncResponse, AsyncSession
 
 
 async def async_request(
+    session: AsyncSession,
     url: str,
-    proxies: str | None = None,
-    pool_maxsize: int = config.POOL_MAXSIZE,
 ) -> AsyncResponse:
-    async with AsyncSession(pool_maxsize=pool_maxsize, timeout=config.TIMEOUT) as session:
-        status_code = 500
-        while status_code == 500:
-            try:
-                response = await session.get(url)
-                status_code == 200
-                return response
-            except Exception as exe:
-                await asyncio.sleep(2)
+
+    for _ in range(config.MAX_CLIENT_REQUESTS):
+        try:
+            return await session.get(url)
+        except Exception:
+            await asyncio.sleep(2)
 
 
 async def request_cycle(
-    urls: lists[str], proxies: str | None = None
+    urls: list[str],
+    proxies: str | None = None,
 ) -> list[AsyncResponse]:
-    tasks = [async_request(url) for url in urls]
-    responses = await asyncio.gather(*tasks)
-    return responses
+
+    async with AsyncSession(
+        pool_maxsize=config.POOL_MAXSIZE,
+        timeout=config.TIMEOUT,
+        proxies=proxies,
+    ) as session:
+
+        tasks = [
+            async_request(session, url)
+            for url in urls
+        ]
+
+        return await asyncio.gather(*tasks)
