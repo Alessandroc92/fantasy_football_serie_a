aaab97512305b64652bce75dda68f886e5f518de
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-09-15 14:44:34 +0200

    Create a structure that returns the parsed HTML instead of other additional components

--- src/fantasy_football_scraper/fetch/browser_fetch.py
+++ src/fantasy_football_scraper/fetch/browser_fetch.py
@@ -1,6 +1,7 @@
 import asyncio
 from itertools import chain
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -24,32 +25,35 @@ def create_driver(driver_path: str):
     )
 
 
-def fetch_player_rating(driver):
-    driver.implicitly_wait(0.5)
+def extract_parsed_html(driver) -> str:
     driver.find_element(By.ID, "disagree-btn").click()
-    player_info = []
-    for team_selector in [".team-home", ".team-away"]:
-        driver.implicitly_wait(0.5)
-        team_button = driver.find_elements(By.CSS_SELECTOR, team_selector)[-1]
-        team_button.click()
-        team_player_info = driver.find_elements(By.CSS_SELECTOR, ".player-info")
-        player_info.append(
-            [
-                rating.get_attribute("innerHTML")
-                for rating in team_player_info
-                if rating.is_displayed()
-            ]
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    visible_ids = set()
+
+    for selector in (".team-away", ".team-home"):
+        button = driver.find_elements(By.CSS_SELECTOR, selector)[-1]
+        button.click()
+
+        visible_ids.update(
+            article.get_attribute("data-r")
+            for article in driver.find_elements(By.CSS_SELECTOR, "article")
+            if article.is_displayed()
         )
-    return list(chain.from_iterable(player_info))
+
+    for article in soup.select("article"):
+        if article.get("data-r") not in visible_ids:
+            article.decompose()
+
+    return str(soup)
+    
 
 
-def browser_extraction(driver_path: str, url: str):
+def browser_extraction(driver_path: str, url: str) -> str:
     try:
         driver = create_driver(driver_path=driver_path)
         driver.get(url=url)
-        html_page = driver.page_source
-        player_rating = fetch_player_rating(driver=driver)
-        return html_page, player_rating
+        return extract_parsed_html(driver=driver)
     finally:
         driver.quit()
 
@@ -67,17 +71,18 @@ async def async_browsers(urls: list[str], driver_path: str):
 if __name__ == "__main__":
     driver_path = resolve_driver_path()
     urls = [
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Venezia-Fiorentina/17994/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Genoa-Frosinone/17987/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Lazio-Milan/17989/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Atalanta-Cagliari/17985/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Lecce-Monza/17990/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Napoli-Bologna/17991/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Sassuolo-Juventus/17992/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Como-Parma/17986/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Torino-Roma/17993/pagelle",
-        "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Inter-Udinese/17988/pagelle",
+        "https://www.fantacalcio.it/serie-a/calendario/3/2026-27/inter-napoli/17980/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Genoa-Frosinone/17987/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Lazio-Milan/17989/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Atalanta-Cagliari/17985/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Lecce-Monza/17990/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Napoli-Bologna/17991/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Sassuolo-Juventus/17992/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Como-Parma/17986/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Torino-Roma/17993/pagelle",
+        # "https://www.fantacalcio.it/serie-a/calendario/4/2026-2027/Inter-Udinese/17988/pagelle",
     ]
 
     res = asyncio.run(async_browsers(urls=urls, driver_path=driver_path))
-    print(res)
+    with open('data/ratings_5.html', 'w') as file:
+        file.write(res[0])
