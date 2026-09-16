a6134a389fa6045dbb0efe4198808a0640665a95
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-09-16 12:45:40 +0200

    Code formatting

--- src/fantasy_football_scraper/services/ratings.py
+++ src/fantasy_football_scraper/services/ratings.py
@@ -5,7 +5,7 @@ from niquests import AsyncResponse
 
 from fantasy_football_scraper import config
 from fantasy_football_scraper.db import save_data
-from fantasy_football_scraper.fetch import fetcher, urls, browser_fetch
+from fantasy_football_scraper.fetch import browser_fetch, fetcher, urls
 from fantasy_football_scraper.parse import html_parsing
 
 
@@ -55,8 +55,8 @@ def extract_teams(match_info: list[dict]) -> set[str]:
 def extract_player_ratings(match_responses: list[str]) -> list[dict]:
     player_ratings = list(
         chain.from_iterable(
-                html_parsing.parse_player_ratings(match_response)
-                for match_response in match_responses
+            html_parsing.parse_player_ratings(match_response)
+            for match_response in match_responses
         )
     )
     return player_ratings
@@ -82,14 +82,16 @@ async def run_extraction_pipeline(
     end_season: int,
     matchday: int | None = None,
     n_matchdays: int = config.MAX_MATCHDAYS,
-) -> dict[str,int]:
+) -> dict[str, int]:
     match_urls = await extract_match_urls(
         start_season=start_season,
         end_season=end_season,
         matchday=matchday,
         n_matchdays=n_matchdays,
     )
-    match_responses = await browser_fetch.async_browsers(urls=match_urls, driver_path=driver_path)
+    match_responses = await browser_fetch.async_browsers(
+        urls=match_urls, driver_path=driver_path
+    )
     match_info = extract_match_info(match_responses)
     teams = extract_teams(match_info)
     player_ratings = extract_player_ratings(match_responses)
@@ -102,21 +104,21 @@ async def run_extraction_pipeline(
     save_data.save_match_info(match_info=match_info)
     save_data.save_player_data(player_data=player_data)
     save_data.save_player_ratings(player_ratings=player_ratings)
-    
+
     return {
-        'teams': len(teams),
-        'matches': len(match_info),
-        'players': len(player_data),
-        'ratings': len(player_ratings)
+        "teams": len(teams),
+        "matches": len(match_info),
+        "players": len(player_data),
+        "ratings": len(player_ratings),
     }
 
 
 if __name__ == "__main__":
     driver_path = browser_fetch.resolve_driver_path()
-    start_season = 2027
-    end_season = 2027
-    matchday = 4
-    n_matchdays = 3
+    start_season = 2016
+    end_season = 2016
+    matchday = None
+    n_matchdays = 38
     recap = asyncio.run(
         run_extraction_pipeline(
             driver_path=driver_path,
@@ -126,4 +128,3 @@ if __name__ == "__main__":
             matchday=matchday,
         )
     )
-    print(recap)
