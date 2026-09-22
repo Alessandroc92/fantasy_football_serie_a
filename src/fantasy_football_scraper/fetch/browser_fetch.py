45a3730a568817fe295e7a3d29fe189e4f345525
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-09-22 10:48:25 +0200

    Split browser interaction in multiple functions to handle different browser flows

--- src/fantasy_football_scraper/fetch/browser_fetch.py
+++ src/fantasy_football_scraper/fetch/browser_fetch.py
@@ -1,22 +1,32 @@
 import asyncio
-from itertools import chain
-import threading
+import os
+from concurrent.futures import ThreadPoolExecutor
 
+import dotenv
 from bs4 import BeautifulSoup
+
+dotenv.load_dotenv()
 from selenium import webdriver
+from selenium.common.exceptions import (
+    ElementClickInterceptedException,
+    StaleElementReferenceException,
+    TimeoutException,
+)
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.common.selenium_manager import SeleniumManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 from fantasy_football_scraper import config
+from fantasy_football_scraper.fetch import proxies
 
-from concurrent.futures import ThreadPoolExecutor
+PROXY_PROVIDER = os.getenv("PROXY_PROVIDER")
+PROXY_PORT = os.getenv("PROXY_PORT")
 
 selenium_executor = ThreadPoolExecutor(
-    max_workers=config.MAX_CONCURRENT_BROWSERS,
-    thread_name_prefix="selenium"
+    max_workers=config.MAX_CONCURRENT_BROWSERS, thread_name_prefix="selenium"
 )
 
 
@@ -26,25 +36,82 @@ def resolve_driver_path() -> str:
 
 
 def create_driver(driver_path: str):
-    service = Service(driver_path,
-    log_output="logs/chromedriver.log",
-    service_args=["--verbose"],
-    popen_kw={"close_fds": False},
-)
+    service = Service(
+        driver_path,
+        log_output="logs/chromedriver.log",
+        service_args=["--verbose"],
+        popen_kw={"close_fds": False},
+    )
+
+    proxy_string = proxies.generate_proxy()
+    proxy = Proxy()
+    proxy.proxy_type = ProxyType.MANUAL
+    proxy.http_proxy = proxy_string
+    proxy.ssl_proxy = proxy_string
+
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
+    options.proxy = proxy
+
     return webdriver.Chrome(
         service=service,
         options=options,
     )
 
 
-def extract_parsed_html(driver) -> str:
-    button = WebDriverWait(
-        driver, config.SELENIUM_STANDARD_TIMEOUT).until(
-            EC.element_to_be_clickable((By.ID, "disagree-btn"))
+def close_ads(driver):
+    try:
+        iframe = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, 'iframe[id*="Smartitial"]')
             )
+        )
+
+        driver.switch_to.frame(iframe)
+
+        close_btn = WebDriverWait(driver, 3).until(
+            EC.element_to_be_clickable((By.ID, "VideoSmartitialCloseBtn"))
+        )
+
+        close_btn.click()
+
+    finally:
+        driver.switch_to.default_content()
+
+
+def regular_flow(driver):
+    for _ in range(config.SELENIUM_BUTTON_ATTEMPTS):
+        try:
+            button = WebDriverWait(driver, config.SELENIUM_STANDARD_TIMEOUT).until(
+                EC.element_to_be_clickable((By.ID, "accept-btn"))
+            )
+            button.click()
+            break
+        except StaleElementReferenceException:
+            continue
+
+
+def alternative_flow(driver):
+    button = WebDriverWait(driver, config.SELENIUM_STANDARD_TIMEOUT).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="CONFIRM"]'))
+    )
     button.click()
+    button = WebDriverWait(driver, config.SELENIUM_STANDARD_TIMEOUT).until(
+        EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, 'button[aria-label="Close success modal"]')
+        )
+    )
+    button.click()
+
+
+def extract_parsed_html(driver) -> str:
+    try:
+        regular_flow(driver=driver)
+    except TimeoutException:
+        alternative_flow(driver=driver)
+    except ElementClickInterceptedException:
+        close_ads(driver=driver)
+        regular_flow(driver=driver)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
     visible_ids = set()
@@ -64,22 +131,31 @@ def extract_parsed_html(driver) -> str:
             article.decompose()
 
     return str(soup)
-    
 
 
 def browser_extraction(driver_path: str, url: str) -> str:
+    driver = create_driver(driver_path=driver_path)
+
     try:
-        driver = create_driver(driver_path=driver_path)
-        driver.get(url=url)
-        return extract_parsed_html(driver=driver)
-    finally:
+        driver.get(url)
+        html = extract_parsed_html(driver=driver)
+
+    except Exception:
+        input("Press ENTER when you're doing inspecting the browser error.")
         driver.quit()
+        raise
+
+    else:
+        driver.quit()
+        return html
 
 
 async def async_browsers(urls: list[str], driver_path: str):
     loop = asyncio.get_running_loop()
 
     async def run(url):
-        return await loop.run_in_executor(selenium_executor, browser_extraction, driver_path, url)
+        return await loop.run_in_executor(
+            selenium_executor, browser_extraction, driver_path, url
+        )
 
     return await asyncio.gather(*(run(url) for url in urls))
